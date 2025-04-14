c64148c4380aa9490e8eaa4b270df1bbf605096c
Arthur Opirata <arthurcarneiro098@gmail.com>
2025-04-14 15:45:37 -0300

    feat: implentação desafio Dio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone:Smartphone
     {
+        public Iphone(string numero, string modelo,string imei, int memoria):base(numero,modelo,imei,memoria)
+        {
+            
+        }
+        public override void InstalarAplicativo(string nomeApp){
+            Console.WriteLine($"iphone está instalou {nomeApp}");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456","modelo 1","1111111111",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("32425","modelo 2","2222222",128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
